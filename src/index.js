aebd69be391f7a06e196f81b649fe8837bf1e393
SonyPc <wpbestcoder@gmail.com>
2022-03-01 11:03:08 +0600

    Update html files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+// eslint-disable-next-line import/no-named-as-default-member
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
